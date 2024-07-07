51e9de040686f1a33e4cb86f4f403684b9dd763f
Pieter Jan Geutjens <pieter.geutjens@leapconsulting.be>
2024-07-07 07:52:41 +0200

    Game class

--- src/lib/game.ts
+++ src/lib/game.ts
@@ -24,4 +24,43 @@ export type GameState = {
     error_pos: Set<number>,
     was_skipped: false,
     first: true,
+    duration: number,
+    accuracy: number,
+    wpm: number,
 };
+
+export class Game {
+    position: number;
+    sequence: Part[];
+    letter_count: number;
+    word_count: number;
+    start_time: number | null;
+    error_pos: Set<number>;
+    was_skipped: boolean;
+    first: boolean;
+    duration: number;
+    accuracy: number;
+    wpm: number;
+
+    constructor(text:string) {
+        this.position = 0;
+        this.sequence = Array.from(text).map((character: string) => ({
+            character,
+            state: State.REMAINING,
+        }));
+        this.error_pos = new Set();
+        this.letter_count = 0;
+        this.word_count = 0;
+        this.start_time = new Date().getTime();
+        this.was_skipped = false;
+
+        this.first = true;
+        this.duration = 0;
+        this.accuracy = 0;
+        this.wpm = 0;
+    }
+
+    get_current() { return this.sequence[this.position]}
+    get_next() {return this.sequence[this.position + 1]}
+    get_at(position: number) { return this.sequence[position]}
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,13 @@
 <script lang="ts">
     import { alphabet } from "$lib/alphabet";
-    import { State, type GameState } from "$lib/game";
-    let currentWordIndex = 0;
+    import { State, Game, type GameState } from "$lib/game";
     let currentLetterIndex = 0;
-    let currentInput = "";
     let correctWords = 0;
-    let startTime;
     let gameActive = false;
     let showStatsOverlay = false;
-    let inputElement;
-    let duration = 0;
-    let accuracy = "";
-    let game_state: GameState;
-    let wpm = "";
+    let game: Game;
+    let duration: number;
+    let accuracy: string, wpm: string
 
     type Settings = {
         ignoreSemicolon: boolean;
@@ -31,149 +26,13 @@ export const test = () => {
     async function startGame() {
         showStatsOverlay = false;
         gameActive = true;
-        game_state = {
-            position: 0,
-            
-            sequence: Array.from(text).map((character: string) => ({
-                character,
-                state: State.REMAINING,
-            })),
-            get_current: () => game_state.sequence[game_state.position],
-            get_next: () => game_state.sequence[game_state.position + 1],
-            get_at: (position: number) => game_state.sequence[position],
-            letter_count: text.length,
-            word_count: 0,
-            done: () => {},
-            start_time: null,
-            error_pos: new Set(),
-            was_skipped: false,
-            first: true,
-        };
-
-        // showStatsOverlay = false;
-        // currentWordIndex = 0;
-        // currentLetterIndex = 0;
-        // gameActive = true;
-        // startTime = new Date().getTime();
-        // currentWordIndex = 0;
-        // currentInput = "";
-        // correctWords = 0;
-        // // await tick();
-        // // inputElement.focus();
-        // let duration = 0;
-    }
-
-    function onkeydown(e: KeyboardEvent) {
-        let current, next
-        console.log("onKyeDown", e.key);
-        // game_state.sequence.forEach((element) =>
-        //   // console.log(element.character, "->", element.character.charCodeAt(0), element.character.length, "->",  element.state),
-        // );
-        e.preventDefault();
-        // const key = e.key.toLowerCase();
-        console.log(e.key);
-        console.log("position:", game_state.position);
-        const last_position = game_state.position;
-        if (e.key.toLowerCase() === "backspace") {
-            console.log("processing backspace");
-            if (game_state.position > 0) {
-                game_state.position--;
-                game_state.sequence[game_state.position].state =
-                    State.REMAINING;
-            }
-        } else if (e.key.toLowerCase() === "enter") {
-            console.log("processing enter");
-            let current = game_state.get_current();
-            let next = game_state.get_next();
-            console.log(current, next);
-            if (!next || !current) {
-                game_state.position++;
-                if (game_state.position >= game_state.sequence.length) {
-                    game_state.word_count++;
-                    endGame();
-                }
-            }
-            if (current.character === ";") {
-                switch (game_settings.ignoreSemicolon) {
-                    case true:
-                        console.log("IGNORING SEMICOLON");
-                        current.state = State.SEMI;
-                        game_state.position++;
-                        break;
-                    case false:
-                        console.log("ERROR SEMICOLON");
-                        current.state = State.INCORRECT;
-                        game_state.error_pos.add(game_state.position);
-                        game_state.position++;
-                        break;
-                }
-                current = game_state.get_current();
-                next = game_state.get_next();
-            }
-
-            while (
-                (current && current.character.charCodeAt(0) === 10) ||
-                current.character.charCodeAt(0) === 32 ||
-                current.character === " "
-            ) {
-                // console.log("SKIP", current.character.charCodeAt(0))
-                current.state = State.CORRECT;
-                game_state.position++;
-                current = game_state.get_current();
-                next = game_state.get_next();
-            }
-        } else if (alphabet.has(e.key)) {
-            console.log("processing letter");
-            current = game_state.get_current();
-            next = game_state.get_next();
-            console.log(current, next);
-            if (current.character === e.key) {
-                current.state = State.CORRECT;
-                if (e.key === " " || e.key === ";") {
-                    game_state.word_count++;
-                }
-                game_state.position++;
-            } else if (e.key === " ") {
-                if (
-                    game_state.position > 0 &&
-                    game_state.get_at(game_state.position - 1).character !== " "
-                ) {
-                    let position = game_state.position;
-                    while (
-                        position < game_state.sequence.length &&
-                        game_state.get_at(position).character !== " "
-                    ) {
-                        game_state.error_pos.add(position);
-                        game_state.get_at(position).state = State.SKIPPED;
-                        game_state.was_skipped = true;
-                        position++;
-                    }
-                    game_state.position = position;
-                    game_state.position++;
-                }
-            } else {
-                current.state = State.INCORRECT;
-                game_state.error_pos.add(game_state.position);
-                game_state.position++;
-            }
-        }
-        if (game_state.position > 0 && game_state.start_time === null) {
-            game_state.start_time = performance.now();
-        }
-        if (game_state.position >= game_state.sequence.length) {
-            if (!game_state.was_skipped) {
-                // for the last word we don't type space so
-                // we count it at the end unless it's skipped
-                game_state.word_count++;
-            }
-            endGame();
-        }
+        game = new Game(text)
     }
 
     function endGame() {
         gameActive = false;
         const endTime = new Date().getTime();
-        duration = (endTime - game_state.startTime) / 1000;
+        duration = (endTime - game.start_time!) / 1000;
 
         const totalWords = (text.split(" ")).length;
         accuracy = ((correctWords / totalWords) * 100).toFixed(2);
@@ -192,6 +51,114 @@ export const test = () => {
         }
         return `${letter}`;
     }
+
+    function onkeydown(e: KeyboardEvent) {
+        let current, next
+        console.log("onKyeDown", e.key);
+        // game.sequence.forEach((element) =>
+        //   // console.log(element.character, "->", element.character.charCodeAt(0), element.character.length, "->",  element.state),
+        // );
+        e.preventDefault();
+        // const key = e.key.toLowerCase();
+        console.log(e.key);
+        console.log("position:", game.position);
+        const last_position = game.position;
+        if (e.key.toLowerCase() === "backspace") {
+            console.log("processing backspace");
+            if (game.position > 0) {
+                game.position--;
+                game.sequence[game.position].state =
+                    State.REMAINING;
+            }
+        } else if (e.key.toLowerCase() === "enter") {
+            console.log("processing enter");
+            let current = game.get_current();
+            let next = game.get_next();
+            console.log(current, next);
+            if (!next || !current) {
+                game.position++;
+                if (game.position >= game.sequence.length) {
+                    game.word_count++;
+                    endGame();
+                }
+            }
+            if (current.character === ";") {
+                switch (game_settings.ignoreSemicolon) {
+                    case true:
+                        console.log("IGNORING SEMICOLON");
+                        current.state = State.SEMI;
+                        game.position++;
+                        break;
+                    case false:
+                        console.log("ERROR SEMICOLON");
+                        current.state = State.INCORRECT;
+                        game.error_pos.add(game.position);
+                        game.position++;
+                        break;
+                }
+                current = game.get_current();
+                next = game.get_next();
+            }
+
+            while (
+                (current && current.character.charCodeAt(0) === 10) ||
+                current.character.charCodeAt(0) === 32 ||
+                current.character === " "
+            ) {
+                // console.log("SKIP", current.character.charCodeAt(0))
+                current.state = State.CORRECT;
+                game.position++;
+                current = game.get_current();
+                next = game.get_next();
+            }
+        } else if (alphabet.has(e.key)) {
+            console.log("processing letter");
+            current = game.get_current();
+            next = game.get_next();
+            console.log(current, next);
+            if (current.character === e.key) {
+                current.state = State.CORRECT;
+                if (e.key === " " || e.key === ";") {
+                    game.word_count++;
+                }
+                game.position++;
+            } else if (e.key === " ") {
+                if (
+                    game.position > 0 &&
+                    game.get_at(game.position - 1).character !== " "
+                ) {
+                    let position = game.position;
+                    while (
+                        position < game.sequence.length &&
+                        game.get_at(position).character !== " "
+                    ) {
+                        game.error_pos.add(position);
+                        game.get_at(position).state = State.SKIPPED;
+                        game.was_skipped = true;
+                        position++;
+                    }
+                    game.position = position;
+                    game.position++;
+                }
+            } else {
+                current.state = State.INCORRECT;
+                game.error_pos.add(game.position);
+                game.position++;
+            }
+        }
+        if (game.position > 0 && game.start_time === null) {
+            game.start_time = performance.now();
+        }
+        if (game.position >= game.sequence.length) {
+            if (!game.was_skipped) {
+                // for the last word we don't type space so
+                // we count it at the end unless it's skipped
+                game.word_count++;
+            }
+            endGame();
+        }
+    }
+
 </script>
 
 <svelte:window on:keydown={onkeydown} />
@@ -215,7 +182,7 @@ export const test = () => {
     {/if}
     <div class="word-list">
         {#if gameActive}
-        {#each game_state.sequence as letter, index}
+        {#each game.sequence as letter, index}
             <letter
                 class="{letter.state} {index === currentLetterIndex
                     ? 'active'
@@ -262,10 +229,6 @@ export const test = () => {
         font-size: 24px;
         padding: 5px;
     }
-    .selected {
-        text-decoration: underline;
-        color: blue;
-    }
     .overlay {
         position: fixed;
         top: 0;
@@ -303,16 +266,16 @@ export const test = () => {
             animation: 1s blink infinite ease-in-out;
         }
         &.correct {
-            color: green;
+            color: var(--green);
         }
         &.incorrect {
-            color: red;
+            color: var(--red);
         }
         &.remaining {
             color: white;
         }
         &.skipped {
-            border-bottom: 1px solid red;
+            border-bottom: 1px solid var(--red);
         }
     }
 
