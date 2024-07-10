01ffcaf42ca33615a08fb006c885eeeb9743ee3c
Pieter Jan Geutjens <pieter.geutjens@leapconsulting.be>
2024-07-10 06:40:36 +0200

    reorder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,13 @@
 <script>
     import '@fortawesome/fontawesome-free/css/all.min.css'
     import "../app.css";
-    import TestConfig from "../components/TestConfig.svelte";
-    import TestStats from "../components/TestStats.svelte";
-    import Test from '../components/Test.svelte';
 </script>
 
 <div class="app-container">
     <header>
         <!-- <Header /> -->
     </header>
-    <TestConfig />
-    <TestStats />
     <main>
-        <Test />
         <slot></slot>
         <!-- Main content will be injected here -->>
     </main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+
+    import Test from "../components/Test.svelte";
+    import TestConfig from "../components/TestConfig.svelte";
+    import TestStats from "../components/TestStats.svelte";
+
+</script>
+<TestConfig />
+<TestStats />
+<Test />
