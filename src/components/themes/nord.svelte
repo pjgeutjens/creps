0aa40a35d0ada82efb9a27f2366a1dd41c6d8eb3
Pieter Jan Geutjens <pieter.geutjens@leapconsulting.be>
2024-07-28 07:47:03 +0200

    feature: themes (start)

---
+++ src/components/themes/nord.svelte
@@ -0,0 +1,16 @@
+// Nord Theme
+
+<svelte:head>
+    <style>
+        :root {
+            --color-scheme: light dark;
+            --dark-background: #1e1e1e;
+            --text-color-bright: #d1d0c5;
+            --text-color-dark: #333;
+            --main-color: #5e81ac;
+            --red: #ca4754;
+            --black: #222;
+            --text-color-subdued: #646669;
+        }
+    </style>
+</svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,19 @@
     import TestStats from "../components/TestStats.svelte";
     import TestHistory from "../components/TestHistory.svelte";
     import TestFuture from "../components/TestFuture.svelte";
+
+    import Nord from "../components/themes/nord.svelte";
+    import Monkey from "../components/themes/monkey.svelte";
+
+    let theme = "monkey";
 </script>
 
+{#if theme === "nord"}
+    <Nord />
+{/if}
+{#if theme === "monkey"}
+    <Monkey />
+{/if}
 <TestConfig />
 <TestHistory />
 <TestFuture />
